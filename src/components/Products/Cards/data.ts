3ac9f21122fe56779f6b5aacb16b81e4546557e5
Yuri Ramos <yurigabriel21@yahoo.com>
2023-09-30 00:32:43 -0300

    Adição de novo produto

--- src/components/Products/Cards/data.ts
+++ src/components/Products/Cards/data.ts
@@ -12,34 +12,46 @@ export const data = [
     {
         id: 2,
 
+        product: 'Petisqueira',
+        description: 'Já pensou em saborear uma petisqueira recheada de pipoca gourmet ?',
+        src:'assets/images/products/Petisqueira.jpg'
+    }, 
+
+
+
+
+
+    {
+        id: 3,
+
         product: 'Ferrero Rocher',
         description: ' Pipocas trufadas com chocolate e avelãs, inspiradas no famoso bombom Ferrero Rocher.',
         src:'assets/images/products/Ferrero.jpg'
     }, 
 
     {
-        id: 3,
+        id: 4,
 
         product: 'Cappuccino',
         description: 'Pipocas com o aroma e o sabor inconfundível do cappuccino, feito para amantes do café.',
         src:'assets/images/products/Cappuccino.jpg'
     }, 
     {
-        id: 4,
+        id: 5,
 
         product: 'Paçoca',
         description: 'Pipocas revestidas com o sabor autêntico da paçoca, uma explosão de amendoim e doçura.',
         src:'assets/images/products/Paçoca.jpg'
     }, 
     {
-        id: 5,
+        id: 6,
 
         product: 'Nutella',
         description: 'Pipocas irresistíveis cobertas com o cremoso sabor de Nutella.',
         src:'assets/images/products/Nutella.jpg'
     }, 
     {
-        id: 6,
+        id: 7,
 
         product: 'Sonho de Valsa',
         description: 'Pipocas com o sabor do bombom mais querido por todos, uma explosão de chocolate.',
@@ -47,7 +59,7 @@ export const data = [
     }, 
 
     {
-        id: 7,
+        id: 8,
 
         product: 'Leite Ninho',
         description: ' Pipocas gourmet cobertas com o delicioso e cremoso sabor do leite em pó Ninho.',
@@ -55,7 +67,7 @@ export const data = [
     }, 
 
     {
-        id: 8,
+        id: 9,
 
         product: 'Cocada',
         description: 'Pipocas saborizadas com coco e leite condensado.',
@@ -63,7 +75,7 @@ export const data = [
     }, 
 
     {
-        id: 9,
+        id: 10,
 
         product: 'Ovomaltine',
         description: 'Pipocas cobertas com a irresistível mistura de chocolate e malted milk Ovomaltine.',
@@ -72,42 +84,42 @@ export const data = [
 
 
     {
-        id: 10,
+        id: 11,
 
         product: 'Tortinha de Limão',
         description: 'Pipocas com um toque cítrico, crocantes e temperadas com limão.',
         src:'assets/images/products/Limão.jpg'
     }, 
     {
-        id: 11,
+        id: 12,
 
         product: 'Doritos',
         description: 'Pipocas temperadas com o sabor audacioso e picante das tortilhas Doritos.',
         src:'assets/images/products/Doritos.jpg'
     }, 
     {
-        id: 12,
+        id: 13,
 
         product: 'Chocolate',
         description: 'Uma indulgência de pipocas cobertas com camadas de chocolate luxuoso.',
         src:'assets/images/products/Chocolate.png'
     }, 
     {
-        id: 13,
+        id: 14,
 
         product: 'Morango',
         description: 'Pipocas doces com sabor intenso de morango, uma explosão frutada a cada mordida.',
         src:'assets/images/products/Morango.jpg'
     }, 
     {
-        id: 14,
+        id: 15,
 
         product: 'Maracujá',
         description: 'Pipocas com um toque tropical, cobertas com o sabor agridoce do maracujá.',
         src:'assets/images/products/Maracujá.jpg'
     }, 
     {
-        id: 15,
+        id: 16,
 
         product: 'Tortinha de Maracujá',
         description: 'Pipocas com o equilíbrio perfeito entre a doçura e um azedinho.',
